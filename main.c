68b8182a87c87677034c8bcd91694d70a33fa8d1
GASPARD <gaspard.dufour@esme.fr>
2024-10-10 17:06:04 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	printf ("Hello hello 2 my beautiful world, colin et caro");
+	printf ("Hello hello 2 my beautiful world, colin et caro poupou");
 	int r = move_forward();
  	int res = move_forward() ;	
 	return 0 ;
